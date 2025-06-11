b09af242f8b55871ed724ba2b86257f4f239f5e0
Anodeluxe <azfanovasammyrafifsaputra@mail.ugm.ac.id>
2025-06-11 21:21:42 +0700

    Updated Catalogue and Accordion

--- data/products.js
+++ data/products.js
@@ -4,6 +4,7 @@ const products = {
     href: "/catalogue/archangel",
     productName: "ARCHANGEL T-Shirt",
     productPrice: "Rp85.000",
+    description: "Illustrates the holy divine of the Archangel, describes what archangel truly is, the divine servant of the Divine God",
     mockups: ["/assets/mockups/archangelFront.png", "/assets/mockups/archangelBack.png",
                 "/assets/mockups/archangelBack.png", "/assets/mockups/archangelFront.png"
     ],
@@ -15,6 +16,7 @@ const products = {
     href: "/catalogue/kyouka",
     productName: "KYOUKA T-Shirt",
     productPrice: "Rp85.000",
+    description: "“Kyouka Suigetsu” (Mirror Flower, Water Moon) is a Japanese idiom referring to something that exists but is not tangible or real",
     mockups: ["/assets/mockups/archangelFront.png", "/assets/mockups/archangelBack.png",
                 "/assets/mockups/archangelBack.png", "/assets/mockups/archangelFront.png"
     ],
@@ -26,6 +28,7 @@ const products = {
     href: "/catalogue/suigetsu",
     productName: "SUIGETSU T-Shirt",
     productPrice: "Rp85.000",
+    description: "“Kyouka Suigetsu” (Mirror Flower, Water Moon) is a Japanese idiom referring to something that exists but is not tangible or real",
     mockups: ["/assets/mockups/archangelFront.png", "/assets/mockups/archangelBack.png",
                 "/assets/mockups/archangelBack.png", "/assets/mockups/archangelFront.png"
     ],
@@ -37,6 +40,7 @@ const products = {
     href: "/catalogue/iaido",
     productName: "IAIDO KATANA T-Shirt",
     productPrice: "Rp85.000",
+    description: "Showcase the art of Japanese swordsmen technique, Iaido, that displays in Brutalism Design Style.",
     mockups: ["/assets/mockups/archangelFront.png", "/assets/mockups/archangelBack.png",
                 "/assets/mockups/archangelBack.png", "/assets/mockups/archangelFront.png"
     ],
@@ -48,6 +52,7 @@ const products = {
     href: "/catalogue/warped",
     productName: "WARPED T-Shirt",
     productPrice: "Rp85.000",
+    description: "get WARPED bro",
     mockups: ["/assets/mockups/archangelFront.png", "/assets/mockups/archangelBack.png",
                 "/assets/mockups/archangelBack.png", "/assets/mockups/archangelFront.png"
     ],
--- src/app/catalogue/[slug]/page.tsx
+++ src/app/catalogue/[slug]/page.tsx
@@ -21,8 +21,9 @@ export default async function ProductPage({ params }: ProductPageProps) {
   return (
     <div className="bg-magnolia relative overflow-hidden">
       <NavBar />
-      <div className="bg-magnolia flex justify-center text-black mt-12 mb-24">
-        <div className="flex flex-row">
+      <div className="bg-magnolia flex flex-col justify-center text-black mt-12">
+        {/* Product Showing */}
+        <div className="flex justify-center flex-row mb-24">
             {/* Images */}
             <div className="relative grid grid-cols-2 w-[40vw] justify-between">
                 {Object.entries(product.mockups).map(([key, mockup], index) => (
@@ -44,18 +45,68 @@ export default async function ProductPage({ params }: ProductPageProps) {
                 ))}
             </div>
             {/* Sidebar */}
-            <div className="pl-16">
-                <h1>{product.productName}</h1>
+            <div className="pl-16 w-[27vw]">
+                <h1 className="font-neoris text-xl mb-4">{product.productName}</h1>
                 <p>Select Size:</p>
-                <button className="border">pura-puranya tombol2 pilih size</button>
-                <p>Ukuran: Unisex XXXXXL</p>
-                <h1>{product.productPrice}</h1>
-                <p>Limited Store Dibuat dari Bahan Bukan Daur Ulang</p>
-                <button className="border">pura-puranya tombol2 pilih banyak yg mau dibeli</button>
+                <button className="bg-thistle border text-2xl">pura-puranya tombol2 pilih size</button>
+                <p className="mb-12">Ukuran: Unisex XXXXXL</p>
+                <h1 className="font-neoris-bold text-2xl">{product.productPrice}</h1>
+                <p className="mb-6">Limited Store Dibuat dari Bahan Bukan Daur Ulang</p>
+                <button className="bg-thistle border w-[50%]">pura-puranya tombol2 pilih banyak yg mau dibeli</button>
                 <p>Stok tersedia</p>
-                <button className="border">Tambahkan ke keranjang</button>
+                <button className="bg-black w-full text-white border mt-16 p-2">Tambahkan ke keranjang</button>
+                <p className="mt-16">{product.description}</p>
             </div>
         </div>
+        {/* Our Other Products */}
+        {/* Header */}
+        <div className="bg-magnolia flex justify-center items-center w-full py-8">
+            <p className="text-8xl text-blackRich font-neotriad leading-none drop-shadow">
+                Our Other Products:
+            </p>
+        </div>
+        {/* Carousel */}
+        <div className="bg-magnolia flex justify-center text-black mb-40">
+            <div className="relative flex w-[70vw] gap-1 justify-between overflow-x-scroll">
+                {/* Products */}
+                {Object.entries(products).map(([key, product], index) => (
+                    <article 
+                        key={index}
+                        className="relative min-w-[300px] overflow-hidden group drop-shadow-lg mb-6"
+                    >
+                        <a href={product.href} className="block w-full h-full">
+                            <div className="relative">
+                            {/* Base image */}
+                            <img
+                                src={product.mockupStatic}
+                                alt="Front"
+                                width="960"
+                                height="480"
+                                className="bg-thistle object-cover w-auto group-hover:opacity-0 transition-opacity duration-300"
+                            />
+                            {/* Hover image */}
+                            <img
+                                src={product.mockupHover}
+                                alt="Back"
+                                width="960"
+                                height="480"
+                                className="bg-thistle object-cover w-auto absolute top-0 left-0 opacity-0 group-hover:opacity-100 transition-opacity duration-300"
+                            />
+                            </div>
+                            <div className="bg-white py-3 px-3">
+                            <h2 className="font-neoris text-xl mb-4">{product.productName}</h2>
+                            <h1 className="text-right font-neoris-bold text-2xl">{product.productPrice}</h1>
+                            </div>
+                        </a>
+                    </article>
+                ))}
+            </div>
+        </div>
+      </div>
+      <div className="bg-black flex justify-center items-center w-full h-[50vh]">
+          <p className="text-[13.6vw] text-magnolia font-neotriad leading-none drop-shadow-xl">
+            pura-puranya ini footer
+          </p>
       </div>
     </div>
   );
